9a801b1aba213f55b0e0da44577896cb39750d23
Muhammad-54107 <Muhammad.qureshi107@gmail.com>
2020-11-10 12:27:43 -0500

    added new stuff on "Hello"

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-
-        System.out.println();
+        // more stuff
+        // mac user and window user are happy to see commit dialog finally
     }
 }
